c01244bc87aa6f6cf0c1b9f6cf8d89d0d8fdd96d
FrancisBilla <francis.billa.fb@gmail.com>
2019-11-20 11:29:21 +0000

    Spring Wed init

---
+++ src/main/java/io/turntabl/NorthwindSpringBootTemplate/NorthwindSpringBootTemplateApplication.java
@@ -0,0 +1,13 @@
+package io.turntabl.NorthwindSpringBootTemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthwindSpringBootTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorthwindSpringBootTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/turntabl/NorthwindSpringBootTemplate/NorthwindSpringBootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package io.turntabl.NorthwindSpringBootTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindSpringBootTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
