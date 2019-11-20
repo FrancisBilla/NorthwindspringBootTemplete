848ad0ab380ba71bffacbab041ded69e0c39272a
FrancisBilla <francis.billa.fb@gmail.com>
2019-11-20 16:11:01 +0000

    adding swagger

--- src/main/java/io/turntabl/NorthwindSpringBootTemplate/Controller/MathsController.java
+++ src/main/java/io/turntabl/NorthwindSpringBootTemplate/Controller/MathsController.java
@@ -1,29 +1,44 @@
 package io.turntabl.NorthwindSpringBootTemplate.Controller;
 
+import io.swagger.annotations.Api;
+import io.turntabl.NorthwindSpringBootTemplate.Maths;
 import io.turntabl.NorthwindSpringBootTemplate.Model.Number;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api
 @RestController
 public class MathsController {
 
-    @RequestMapping("/add")
+    @Autowired
+    private Maths mathOperation;
+
+
+    @GetMapping("/add")
     public Number add(
             @RequestParam(required = true, defaultValue = "1")Integer num1,
             @RequestParam(required = true, defaultValue = "1")Integer num2){
 
-        return new Number(num1 + num2) {
-        };
+            Number sum = mathOperation.add(num1,num2);
+            //Number number =new Number();
+            //number.setValue(sum);
+
+        return  sum;
     }
 
 
-    @RequestMapping("/subtract")
+    @GetMapping("/subtract")
     public Number subtract(
             @RequestParam(required = true, defaultValue = "2")Integer num1,
             @RequestParam(required = true, defaultValue = "1")Integer num2){
-        // int value = new
-         return new Number(num1 - num2);
+        Number subt = mathOperation.subtract(num1,num2);
+         return subt;
     }
+
+
 
 }
--- src/main/java/io/turntabl/NorthwindSpringBootTemplate/NorthwindSpringBootTemplateApplication.java
+++ src/main/java/io/turntabl/NorthwindSpringBootTemplate/NorthwindSpringBootTemplateApplication.java
@@ -2,7 +2,10 @@ package io.turntabl.NorthwindSpringBootTemplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class NorthwindSpringBootTemplateApplication {
 
@@ -11,4 +14,6 @@ public class NorthwindSpringBootTemplateApplication {
 		SpringApplication.run(NorthwindSpringBootTemplateApplication.class, args);
 	}
 
+	@Bean
+	public Maths math(){return new Maths();}
 }
